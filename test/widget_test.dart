dafd62be58c5d7e91e9d5ac0c3eb00015095c57c
DadanHidayatulloh <dadanhidayatulloh22@gmail.com>
2025-10-23 14:49:47 +0700

    m

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:rekamuktiputra/main.dart';
+import 'package:rekamukti_putra/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
